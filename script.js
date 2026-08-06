9fe22a3ec27b71dbe8d9d23ca0b33ffa3e453d08
harveymeale9 <harveymeale9@gmail.com>
2026-08-06 13:37:13 +0700

    d

--- script.js
+++ script.js
@@ -802,4 +802,5 @@ async function init() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", init);
+if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", init);
+else init();
